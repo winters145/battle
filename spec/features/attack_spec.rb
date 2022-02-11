e47983843b0745b9a192022bb8ced86c7c94119c
Jack <winterschladen.j@gmail.com>
2022-02-11 16:45:57 +0000

    Added feature test of a 'Player LOSES' message when hit_points reach zero.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,18 @@
-feature 'Attack player 2' do
+require_relative '../../app.rb'
+require_relative '../../lib/player.rb'
+require_relative '../../lib/game.rb'
+
+feature 'Attacking' do
   scenario 'Player 1 attacks player 2' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Dash attacked Chloe'
+  end
+
+  scenario 'Player 2 hit points reach zero' do
+    sign_in_and_play
+    18.times { (click_button 'Attack') && (click_button 'Return') }
+    click_button 'Attack'
+    expect(page).to have_content 'Chloe LOSES!'
   end
 end
